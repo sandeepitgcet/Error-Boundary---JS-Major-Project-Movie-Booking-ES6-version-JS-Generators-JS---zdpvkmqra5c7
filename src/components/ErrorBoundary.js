610528dfb62e3a5405ae2abf401ebd5640116478
Sandeep Singh <33353409+sandeepitgcet@users.noreply.github.com>
2023-01-06 15:31:59 +0000

    updated

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
  
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          
+          Some thing went wrong
         </p>
       );
     }
