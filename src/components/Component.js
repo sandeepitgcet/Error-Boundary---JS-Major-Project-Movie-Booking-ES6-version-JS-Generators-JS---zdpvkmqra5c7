18245114518c6a63cf7b2e6dede9af5f4679ae1b
Sandeep Singh <33353409+sandeepitgcet@users.noreply.github.com>
2023-01-08 13:53:03 +0000

    updated

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,7 +4,11 @@ const Component = (props) => {
     
     const [error, setError] = useState(true);
   useEffect(()=>{
+    
     setError(!error)
+    if(!error){
+      throw Error('Error')
+    }
   },[props.change])
   return (
     <>
